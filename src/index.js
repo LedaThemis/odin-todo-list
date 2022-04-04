36584014a0c261819553a1c95bc18a1e7ed25f26
Leda <ledathemis@protonmail.com>
2022-04-04 19:25:31 +0300

    make projects dropdown

--- src/index.js
+++ src/index.js
@@ -72,6 +72,18 @@ const storage = (() => {
   };
 })();
 
+const createOptionElement = (name, value) => {
+  const option = document.createElement('option');
+  option.innerText = name;
+  option.value = value;
+
+  return option;
+};
+
+const getProjectsDropdownValues = (projects) => {
+  return projects.map((project, i) => createOptionElement(project, i));
+};
+
 const showAddTaskForm = () => {
   const addTaskFormDiv = document.querySelector('#add-task-div');
   addTaskFormDiv.style.display = 'grid';
@@ -82,7 +94,30 @@ const hideAddTaskForm = () => {
   addTaskFormDiv.style.display = 'none';
 };
 
+const showAddProjectForm = () => {
+  const addProjectFormDiv = document.querySelector('#add-project-div');
+  addProjectFormDiv.style.display = 'grid';
+};
+
+const hideAddProjectForm = () => {
+  const addProjectFormDiv = document.querySelector('#add-project-div');
+  addProjectFormDiv.style.display = 'none';
+};
+
 function handleAddTask(e) {
+  const projectsDropdown = document.querySelector('#task-project');
+  const dropdownValues = getProjectsDropdownValues(storage.getProjects());
+
+  projectsDropdown.replaceChildren();
+
+  const emptyOption = createOptionElement('', '');
+  emptyOption.selected = true;
+  emptyOption.disabled = true;
+  projectsDropdown.appendChild(emptyOption);
+
+  dropdownValues.forEach((dropdownValue) =>
+    projectsDropdown.appendChild(dropdownValue)
+  );
   showAddTaskForm();
 }
 
@@ -91,27 +126,70 @@ function handleCloseForm(e) {
 }
 
 function handleSubmitTask(e) {
-  const formData = new FormData(document.querySelector('#add-task-form'));
+  const form = document.querySelector('#add-task-form');
+  const formData = new FormData(form);
   const title = formData.get('task-name');
   const dueDate = formData.get('task-dueDate');
   const priority = formData.get('task-priority');
-  const project = formData.get('task-project');
+  const projectId = formData.get('task-project');
 
   const p = document.querySelector('#fill-all-required-fields');
-  if (!title || !dueDate | !priority || !project) {
+  if (!title || !dueDate | !priority || !projectId) {
     p.textContent = 'Please fill all the fields.';
     return;
   }
   p.textContent = '';
 
-  const task = createTask(title, dueDate, priority, project);
+  const task = createTask(title, dueDate, priority);
+
+  storage.addTask(task, projectId);
+
+  form.reset();
+  hideAddTaskForm();
+
+  console.log(storage.getTasks());
+}
+
+function handleAddProject(e) {
+  showAddProjectForm();
+}
+
+const handleCloseProjectForm = () => {
+  hideAddProjectForm();
+};
+
+function handleSubmitProject(e) {
+  const form = document.querySelector('#add-project-form');
+  const formData = new FormData(form);
+  const name = formData.get('project-name');
+
+  const p = document.querySelector('#fill-project-name');
+  if (!name) {
+    p.textContent = 'Please provide a project name.';
+    return;
+  }
+  p.textContent = '';
+
+  storage.addProject(name);
+
+  form.reset();
+  hideAddProjectForm();
 }
 
 const addTaskButton = document.querySelector('#add-task');
 addTaskButton.addEventListener('click', handleAddTask);
 
-const closeFormButton = document.querySelector('#close-form');
+const closeFormButton = document.querySelector('#close-task-form');
 closeFormButton.addEventListener('click', handleCloseForm);
 
 const submitTaskButton = document.querySelector('#submit-task');
 submitTaskButton.addEventListener('click', handleSubmitTask);
+
+const addProjectButton = document.querySelector('#add-project');
+addProjectButton.addEventListener('click', handleAddProject);
+
+const closeProjectForm = document.querySelector('#close-project-form');
+closeProjectForm.addEventListener('click', handleCloseProjectForm);
+
+const submitProjectButton = document.querySelector('#submit-project');
+submitProjectButton.addEventListener('click', handleSubmitProject);
