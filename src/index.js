61b33b164a9f82d45e30e76ac083f7b8c7d1e135
Leda <ledathemis@protonmail.com>
2022-04-04 17:38:24 +0300

    add todo logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,73 @@
 import './styles.css';
-import clearIcon from './clear_icon.svg';
-import editIcon from './edit_icon.svg';
 
-const deleteButton = document.querySelector('#delete-task');
+const createTask = (title, dueDate, priority) => {
+  let isDone = false;
+  let _priority = priority;
 
-deleteButton.src = clearIcon;
+  const setDone = () => {
+    isDone = true;
+  };
 
-const editButton = document.querySelector('#edit-task');
+  const setDoing = () => {
+    isDone = false;
+  };
 
-editButton.src = editIcon;
+  const getIsDone = () => {
+    return isDone;
+  };
+
+  const setPriority = (newPriority) => {
+    _priority = newPriority;
+  };
+
+  const getPriority = () => {
+    return _priority;
+  };
+
+  return {
+    title,
+    dueDate,
+    setDone,
+    setDoing,
+    getIsDone,
+    getPriority,
+    setPriority,
+  };
+};
+
+const storage = (() => {
+  let projects = ['default'];
+  let tasks = [];
+
+  const addProject = (name) => {
+    projects.push(name);
+  };
+  const removeProject = (id) => {
+    projects = projects.slice(0, id).concat(projects.slice(id + 1));
+  };
+
+  const getProjects = () => {
+    return projects;
+  };
+
+  const addTask = (task, projectId) => {
+    tasks.push({ task, projectId });
+  };
+
+  const removeTask = (id) => {
+    tasks = tasks.slice(0, id).concat(tasks.slice(id + 1));
+  };
+
+  const getTasks = () => {
+    return tasks;
+  };
+
+  return {
+    addTask,
+    removeTask,
+    getTasks,
+    addProject,
+    removeProject,
+    getProjects,
+  };
+})();
